16cb0c6207789c915964780e7720d493099e2329
Lionel Francis <lionelishmael@gmail.com>
2025-08-08 19:26:22 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App() {
             isAuthenticated ? <Navigate to="/dashboard" replace /> : <LoginPage />
           } />
           
+          
           {/* Display client route */}
           <Route path="/display" element={<DisplayClient />} />
           
